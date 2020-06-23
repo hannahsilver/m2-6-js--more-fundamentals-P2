f1f293c07dccbc6634db953de20a3a6857355ac1
Hannah Silver <hannah_silver@hotmail.com>
2020-06-23 17:16:39 -0400

    exercise 7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,22 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
-// createRecommendations function
-
-// printRecommendations function
-
-// function call (done)
+function createRecommendations(obj) {
+  const allKeys = Object.keys(obj);
+  const newArr = allKeys.map(function (key) {
+    return `With ${key} it is best to have ${obj[key]}.`;
+  });
+  return newArr;
+}
+function printRecommendations(arr) {
+  arr.forEach(function (string) {
+    console.log(string);
+  });
+}
 printRecommendations(createRecommendations(foodPairings));
